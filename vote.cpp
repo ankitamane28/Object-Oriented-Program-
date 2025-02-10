62f0f48009c9b633f36f2bb5535664911079758b
ankitamane28 <maneanku451@gmail.com>
2025-02-10 21:16:06 +0530

    Create vote.cpp

---
+++ vote.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+
+    cin>>age;
+    if(age<=18)
+    {
+        cout<<"the voter is valid to the vote"<<endl;
+
+    }
+    else{
+        cout<<"the voter is not valid to the vote"<<endl;
+    }
+return 0;
+}
